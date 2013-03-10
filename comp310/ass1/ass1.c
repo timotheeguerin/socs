93da0d1647a36c08f8d31a342cb4fe6ad3cec5e6
timcolonel <timothee.guerin@outlook.com>
2013-03-10 13:34:46 -0400

    Assignment 1

---
+++ comp310/ass1/ass1.c
@@ -0,0 +1,113 @@
+/*
+ * =============================================================================
+ *
+ *      Filename:  my-test1.c
+ *
+ *   	Description:  Simple tester for MyThread - Test 1.
+ *
+ *      Version:   1.1
+ *      Created:   03/08/2012 11:09:44 AM
+ *      Compiler:  gcc
+ *
+ *      Author:    Xing Shi Cai
+ *      Revisions: Devarun Bhattacharya   
+ *                 M. Maheswaran
+ * =============================================================================
+ */
+/* Includes */
+#include <stdio.h>      /* Input/Output */
+#include <stdlib.h>     /* General Utilities */
+#include <math.h>
+
+#include "mythread.h"
+
+/* prototype for thread routine */
+void handler ( );
+
+/* global vars */
+
+/* semaphores are declared global so they can be accessed 
+   in main() and in thread routine,
+   here, the semaphore is used as a mutex */
+
+int counter_mutex;
+
+/* shared variables */
+int counter; 
+double result = 0.0;
+
+int main()
+{
+    int thread_num = 10;
+    int j;
+    char* thread_names[] = {
+        "thread 0",
+        "thread 1",
+        "thread 2",
+        "thread 3",
+        "thread 4",
+        "thread 5",
+        "thread 6",
+        "thread 7",
+        "thread 8",
+        "thread 9"
+    };
+
+    /* Initialize MyThreads library. */
+    mythread_init();
+
+    /* 250 ms */
+    set_quantum_size(5000);
+
+    counter_mutex = create_semaphore(1);
+
+    for(j=0; j<thread_num; j++)
+    {
+        mythread_create(thread_names[j], (void *) &handler, 6004);
+    }
+
+    /* Print threads informations before run */
+    mythread_state();
+
+    /* When this function returns, all threads should have exited. */
+    runthreads();
+    printf("Run ended\n");
+
+    destroy_semaphore(counter_mutex);
+
+    /* Print threads informations after run */
+    mythread_state();
+
+    printf("The counter is %d\n", counter);
+    printf("The result is %f\n", result);
+
+    if (counter == 50 &&
+	(result - 151402.656521) < 0.000001)
+      printf(">>> Thread library PASSED the Test 1\n");
+
+    exit(0);
+}
+
+
+void handler ()
+{
+    int i;
+    for(i=0; i < 5; i++)
+    {
+        /* If you remove this protection, you should be able to see different
+         * out of every time you run this program.  With this protection, you
+         * should always be able to see result to be 151402.656521 */
+        semaphore_wait(counter_mutex);       /* down semaphore */
+
+        /* START CRITICAL REGION */
+        int j;
+        for (j = 0; j < 1000; j++) {
+            result = result + sin(counter) * tan(counter);
+        }
+        counter++;
+        /* END CRITICAL REGION */    
+
+        semaphore_signal(counter_mutex);       /* up semaphore */
+    }
+    mythread_exit(); /* exit thread */
+}
---
+++ comp310/ass1/mythread.c
@@ -0,0 +1,316 @@
+#include "mythread.h"
+
+int threadCounter, current_thread, quantum_size, end, active_semaphores;
+long long int switch_ctr;
+List * runQueue;
+mythread_control_block thread_table[THREAD_MAX_SIZE];
+Semaphore semaphore_table[SEMAPHORE_MAX_SIZE];
+ucontext_t uctx_main;
+
+long getCurrentNanoTime()
+{
+	struct timespec cur_time;
+	clock_gettime(CLOCK_REALTIME, &cur_time);
+	return cur_time.tv_nsec;
+}
+int mythread_init()
+{
+	int i;
+	for(i = 0; i < THREAD_MAX_SIZE; i++)
+	{
+		thread_table[i].state = NEW;
+	}
+	runQueue = list_create(NULL);
+	threadCounter = 0;
+	current_thread = 0;
+	quantum_size = 0;
+	active_semaphores = 0;
+	switch_ctr = 0;
+	end = 0;
+
+	if(runQueue == 0)
+	{
+		return -1;
+	}
+
+	return 0;
+}
+
+int mythread_create(char *threadname, void (*threadfunc)(), int stacksize)
+{
+	int pointer = threadCounter;
+
+	if(threadCounter >= THREAD_MAX_SIZE)
+	{
+		return -1;
+	}
+
+	mythread_control_block thread;
+
+	getcontext(&thread_table[pointer].context);
+
+	thread_table[pointer].context.uc_link = &uctx_main;
+	thread_table[pointer].stack = malloc(stacksize);
+	thread_table[pointer].context.uc_stack.ss_sp = thread_table[pointer].stack;
+	thread_table[pointer].context.uc_stack.ss_size = stacksize;
+	thread_table[pointer].context.uc_stack.ss_flags = 0;
+	sigemptyset(&thread_table[pointer].context.uc_sigmask);
+
+
+
+	thread_table[pointer].thread_name = threadname;
+	thread_table[pointer].thread_id = pointer;
+	thread_table[pointer].state = RUNNABLE;
+	//Time only count when thread is running
+	thread_table[pointer].start_time = 0;
+
+	if(thread_table[pointer].stack == 0)
+	{
+		printf("Stack not allocated\n");
+		return -1;
+	}
+
+	makecontext(&thread_table[pointer].context, threadfunc, 0);
+
+
+	//Finally add the thread to the running queue
+	runQueue = list_append_int(runQueue, pointer);
+
+
+	threadCounter ++;
+	return pointer ;
+}
+
+void mythread_exit()
+{
+	thread_table[current_thread].state = EXIT;
+	long end_time = getCurrentNanoTime();
+	thread_table[current_thread].running_time += end_time - thread_table[current_thread].start_time;
+}
+
+void switch_thread()
+{
+	switch_ctr++;
+	//If the running queue is empty and current thread is exit
+	if(list_empty(runQueue) && thread_table[current_thread].state == EXIT)
+	{
+		//Calculate the running time
+		long end_time = getCurrentNanoTime();
+		thread_table[current_thread].running_time += end_time - thread_table[current_thread].start_time;
+		end = 1;
+		setcontext(&uctx_main);
+	}
+	//If we have a another thread in the running queue we switch
+	if(!list_empty(runQueue))
+	{
+		//Switch thread with the first in the queue
+		int old_thread = current_thread;
+		current_thread = list_shift_int(runQueue);
+
+		//Update the running time of the old thread
+		long end_time = getCurrentNanoTime();
+		thread_table[old_thread].running_time += end_time - thread_table[old_thread].start_time;
+
+		//Reset the start time of the current_thread
+		thread_table[current_thread].start_time = getCurrentNanoTime();
+
+		//If we still need to run the old thread we put it back in the running quue 
+		if(thread_table[old_thread].state == RUNNABLE || thread_table[old_thread].state == RUNNING)
+		{
+			runQueue = list_append_int(runQueue, old_thread);
+		}
+
+		//Update the context
+		if(swapcontext(&thread_table[old_thread].context, &thread_table[current_thread].context) == -1)
+		{
+			printf("Swap context failed\n");
+			return;
+		}
+	}
+	else //No other thread in the queue
+	{
+		if(swapcontext(&thread_table[current_thread].context, &uctx_main) == -1)
+		{
+			return;
+		}
+	}
+}
+void runthreads()
+{
+
+	sigset_t x;
+	sigemptyset(&x);
+	sigaddset(&x, SIGALRM);
+	sigprocmask(SIG_BLOCK, &x, NULL);
+	//If we have no thread quit
+	if(threadCounter == 0)
+	{
+		return;
+	}
+
+	//Setup the quantum timer
+	struct itimerval timer;
+	timer.it_interval.tv_sec = 0;
+	timer.it_interval.tv_usec = quantum_size;
+	timer.it_value.tv_sec = 0;
+	timer.it_value.tv_usec = quantum_size;
+	setitimer(ITIMER_REAL, &timer, 0);
+
+	sigset(SIGALRM, &switch_thread);
+
+	//Pop the first thread to run
+	current_thread = list_shift_int(runQueue);
+	//Set its state to RUNNING
+	thread_table[current_thread].state = RUNNING;
+	//Set the start_running time
+	thread_table[current_thread].start_time = getCurrentNanoTime();
+
+	sigprocmask(SIG_UNBLOCK, &x, NULL);
+
+	if(swapcontext(&uctx_main, &thread_table[current_thread].context) == -1)
+	{
+		return;
+	}
+
+	while(!list_empty(runQueue))
+	{
+		printf("Run\n");
+		current_thread = list_shift_int(runQueue);
+		thread_table[current_thread].state = RUNNING;
+
+		thread_table[current_thread].start_time = getCurrentNanoTime();
+
+		if(swapcontext(&uctx_main, &thread_table[current_thread].context) == -1)
+		{
+			return;
+		}
+	}
+
+	sigprocmask(SIG_BLOCK, &x, NULL);
+
+	int i;
+	for(i = 0; i < threadCounter; i++)
+	{
+		thread_table[i].state = EXIT;
+	}
+}
+
+
+void set_quantum_size(int q)
+{
+	quantum_size = q;
+}
+
+int create_semaphore(int value)
+{
+	printf("create Sem\n");
+	if(active_semaphores >= SEMAPHORE_MAX_SIZE)
+	{
+		return -1;
+	}
+
+	int pointer = active_semaphores;
+
+	semaphore_table[pointer].value = value;
+	semaphore_table[pointer].initial = value;
+	semaphore_table[pointer].queue = list_create(NULL);
+
+	active_semaphores++;
+	return pointer;
+}
+
+void semaphore_wait(int semaphore)
+{
+	sigset_t set;
+	sigemptyset(&set);
+	sigaddset(&set,SIGALRM);
+	sigprocmask(SIG_BLOCK,&set,NULL);
+
+	long long int old_switch_ctr = switch_ctr; 
+
+	semaphore_table[semaphore].value --;
+
+	if(semaphore_table[semaphore].value < 0)
+	{
+		thread_table[current_thread].state = BLOCKED;
+		list_append_int(semaphore_table[semaphore].queue, current_thread);
+	}
+	sigprocmask(SIG_UNBLOCK,&set,NULL);
+
+	while(old_switch_ctr==switch_ctr);
+}
+
+void semaphore_signal(int semaphore)
+{
+
+	//Init signal
+	sigset_t x;
+	sigemptyset(&x);
+	sigaddset(&x, SIGALRM);
+
+	//Block signal
+	sigprocmask(SIG_BLOCK, &x, NULL);
+
+	semaphore_table[semaphore].value++;
+
+	if(semaphore_table[semaphore].value <= 0)
+	{
+		int thread;
+		thread = list_shift_int(semaphore_table[semaphore].queue);
+		thread_table[thread].state = RUNNABLE;
+
+		//Put back the thread in the running queue
+		runQueue = list_append_int(runQueue, thread);
+	} 
+
+	sigprocmask(SIG_UNBLOCK, &x, NULL);
+	switch_thread();
+}
+
+void destroy_semaphore(int semaphore)
+{
+	if(!list_empty(semaphore_table[semaphore].queue))
+	{
+		return;
+	}
+	if(semaphore_table[semaphore].initial != semaphore_table[semaphore].value)
+	{
+		return;
+	}
+}
+
+void mythread_state()
+{
+	printf("Thread state: \n");
+	printf("%-20s%-10s %s\n", "Name","State", "Time");
+
+	int i;
+	for(i = 0; i < threadCounter; i++)
+	{
+		char * state;
+		switch(thread_table[i].state)
+		{
+		case NEW:
+			state = "NEW";
+			break;
+		case RUNNING:
+			state = "RUNNING";
+			break;
+		case RUNNABLE:
+			state = "RUNNABLE";
+			break;
+		case BLOCKED:
+			state = "BLOCKED";
+			break;
+		case EXIT:
+			state = "EXIT";
+			break;
+		default:
+			state = "UNDEFINED";
+			break;
+		}
+		printf("%-20s%-10s %lu\n", thread_table[i].thread_name, state, thread_table[i].running_time);
+	}
+
+	printf("\n");
+}

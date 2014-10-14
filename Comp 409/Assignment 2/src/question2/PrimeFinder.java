94298abfc1ab70fc5fc76a80360d4aab2d55b7b6
Timothee Guerin <timothee.guerin@outlook.com>
2014-10-14 11:41:33 -0400

    prime finder cleanup

--- Comp 409/Assignment 2/src/question2/PrimeFinder.java
+++ Comp 409/Assignment 2/src/question2/PrimeFinder.java
@@ -14,52 +14,49 @@ public class PrimeFinder {
     public BigInteger n;
     public int p;
 
-    BigInteger two = new BigInteger("2");
+    private static final BigInteger TWO = BigInteger.valueOf(2L);
 
     private List<BigInteger> factors = new ArrayList<BigInteger>();
 
     public static void main(String[] args) {
-        PrimeFinder finder = new PrimeFinder();
-        finder.n = new BigInteger("2532648290334938494");
-        finder.p = 16;
-        int sample_size = 1;
-        int sum = 0;
-        for (int i = 0; i < sample_size; i++) {
-            long start = System.currentTimeMillis();
-            finder.getPrimeFactor();
-            long end = System.currentTimeMillis();
-            sum += (end - start);
-            System.out.println(end - start);
+        if (args.length < 2) {
+            System.out.println("Error missing arguments: expecting 2.");
             return;
         }
-        System.out.println(sum / sample_size);
+        PrimeFinder finder = new PrimeFinder();
+        finder.n = new BigInteger(args[0]);
+        finder.p = Integer.parseInt(args[1]);
+        long start = System.currentTimeMillis();
+        finder.getPrimeFactor();
+        long end = System.currentTimeMillis();
+        System.out.println(end - start);
     }
 
-    public void getPrimeFactor() {
+    public List<BigInteger> getPrimeFactor() {
 
-        if (n.mod(two).equals(BigInteger.ZERO)) {
-            factors.add(two);
+        if (n.mod(TWO).equals(BigInteger.ZERO)) {
+            factors.add(TWO);
         }
 
         Thread[] threads = new Thread[p];
         BigInteger max = sqrt(n);
         for (int i = 0; i < p; i++) {
-            BigInteger val = new BigInteger(String.valueOf(i * 2 + 3));
+            //Each thread is given a starting index
+            BigInteger val = BigInteger.valueOf(i * 2 + 3);
             threads[i] = new Thread(new PrimeFinderThread(val, max));
         }
 
-        for (Thread thread : threads) {
-            thread.start();
-        }
+        for (Thread thread : threads) thread.start();
 
-        for (Thread thread : threads) {
-            try {
-                thread.join();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+        try {
+            for (Thread thread : threads) thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
+        //Sort factors as they could have been inserted in the wrong order
+        //e.g. the thread testing 5 run before the thread testing 3 for 15
         Collections.sort(factors);
+        //Only keep prime numbers(not divisible by any of the other numbers)
         List<BigInteger> results = new ArrayList<BigInteger>();
         for (int i = 0; i < factors.size(); i++) {
             boolean found = false;
@@ -75,11 +72,14 @@ public class PrimeFinder {
             }
         }
 
+        System.out.println("Prime factors: ");
         for (BigInteger val : results) {
             System.out.print(val + "  ");
         }
-        System.out.println();
+        System.out.println("\n");
+        return results;
     }
+
 
     class PrimeFinderThread implements Runnable {
         private BigInteger current;
@@ -87,13 +87,14 @@ public class PrimeFinder {
         private BigInteger offset;
 
         public PrimeFinderThread(BigInteger current, BigInteger max) {
-            this.current = current;
-            this.max = max;
-            this.offset = new BigInteger(String.valueOf(p * 2));
+            this.current = current; //Current divisor to check
+            this.max = max; //Max value to check
+            this.offset = new BigInteger(String.valueOf(p * 2)); //Skip offset values each divisor
         }
 
         @Override
         public void run() {
+            //Each thread try each value every offset
             while (current.compareTo(max) == -1) {
                 if (n.mod(current).equals(BigInteger.ZERO)) {
                     factors.add(current);
@@ -105,6 +106,7 @@ public class PrimeFinder {
 
 
     /**
+     * Helper function.
      * Computes the square root of a big integer
      *
      * @throws IllegalArgumentException
@@ -115,15 +117,17 @@ public class PrimeFinder {
         }
 
         // square roots of 0 and 1 are trivial
-        if (x == BigInteger.ZERO || x == BigInteger.ONE) {
+        if (x.equals(BigInteger.ZERO) || x.equals(BigInteger.ONE)) {
             return x;
         }
 
-        BigInteger two = BigInteger.valueOf(2L);
+
         BigInteger y;
 
         // starting with y = x / 2 avoids magnitude issues with x squared
-        for (y = x.divide(two); y.compareTo(x.divide(y)) > 0; y = ((x.divide(y)).add(y)).divide(two)) ;
+        for (y = x.divide(TWO); y.compareTo(x.divide(y)) > 0; ) {
+            y = ((x.divide(y)).add(y)).divide(TWO);
+        }
 
         return y;
     }
